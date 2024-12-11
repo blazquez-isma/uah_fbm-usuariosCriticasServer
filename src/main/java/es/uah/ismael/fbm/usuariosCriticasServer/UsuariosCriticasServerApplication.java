4abde6ee44e76595a867f532c1af2cfe4afd069b
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2024-12-11 22:48:54 +0100

    Creación del proyecto

---
+++ src/main/java/es/uah/ismael/fbm/usuariosCriticasServer/UsuariosCriticasServerApplication.java
@@ -0,0 +1,13 @@
+package es.uah.ismael.fbm.usuariosCriticasServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosCriticasServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosCriticasServerApplication.class, args);
+	}
+
+}
